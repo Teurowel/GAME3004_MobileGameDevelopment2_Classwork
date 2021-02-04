2b439363ba31c9784726297dd07b815998468041
DoosungJang <teurowel@gmail.com>
2021-02-05 02:29:51 +0900

    Lab2

--- Assets/_Scripts/CameraController.cs
+++ Assets/_Scripts/CameraController.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public float mouseSensitivity = 1000.0f;
+    public float sensitivity = 1000.0f;
     public Transform playerBody;
 
-    private float XRotation = 0.0f;
+    private float XAxisRotation = 0.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,13 +18,15 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
 
-        XRotation -= mouseY;
-        XRotation = Mathf.Clamp(XRotation, -90.0f, 90.0f);
-
-        transform.localRotation = Quaternion.Euler(XRotation, 0.0f, 0.0f);
+        // Look up and down
+        XAxisRotation -= mouseY;
+        XAxisRotation = Mathf.Clamp(XAxisRotation, -90.0f, 90.0f);
+        transform.localRotation = Quaternion.Euler(XAxisRotation, 0.0f, 0.0f);
+        
+        // Look left and right and rotate around the Y Axis
         playerBody.Rotate(Vector3.up * mouseX);
     }
 }
--- Assets/_Scripts/DeathPlaneController.cs
+++ Assets/_Scripts/DeathPlaneController.cs
@@ -6,18 +6,13 @@ public class DeathPlaneController : MonoBehaviour
 {
     public Transform spawnPoint;
 
-    private void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
-            Debug.Log("DeathPlaneController: Collision with Player");
-
-            //CharacterController doesn't allow to set trasform.position, so we need to disable it first
-            var player = other.gameObject.GetComponent<PlayerBehavior>();
+            var player = other.gameObject.GetComponent<PlayerBehaviour>();
             player.controller.enabled = false;
-
-            other.gameObject.transform.position = spawnPoint.position;
-
+            player.transform.position = spawnPoint.position;
             player.controller.enabled = true;
         }
     }
